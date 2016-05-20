3a8cc9e8f62add3ae35a0f7bfd75dd4f017a8728
Diachenko Dmytro <dimatrump@gmail.com>
2016-05-20 07:18:59 -0700

    modified dynamic

--- students/views.py
+++ students/views.py
@@ -40,7 +40,7 @@ def students_delete(request, sid):
 # Views for groups
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups List</h1>')
+	return render(request, 'students/groups_list.html', {})
 	
 def groups_add(request):
 	return HttpResponse('<h1>Add Group Form</h1>')
